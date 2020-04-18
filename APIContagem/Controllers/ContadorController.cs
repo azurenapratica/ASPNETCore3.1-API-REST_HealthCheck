f83feee36176ba1f4fc0a1f8fd9470fcd3b0d91d
Milton Câmara <miltoncamaragomes@gmail.com>
2020-04-18 10:20:05 -0300

    Atualizado contador para versão 2

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Teste",
+                    Local = "Teste Versão 2",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
